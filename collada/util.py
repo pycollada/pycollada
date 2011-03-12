ad8c5adb9c8950a41fcc0a525bb3f8e6822cba95
Jeff Terrace <jterrace@gmail.com>
2011-03-11 19:39:08 -0500

    Added util function for normalizing a n,3 vector

--- collada/util.py
+++ collada/util.py
@@ -24,3 +24,10 @@ def checkSource( source, components, maxindex):
     if source.components != components:
         raise DaeMalformedError('Wrong format in source %s'%source.id)
     return source
+
+def normalize_v3(arr):
+    ''' Normalize a numpy array of 3 component vectors shape=(n,3) '''
+    lens = numpy.sqrt( arr[:,0]**2 + arr[:,1]**2 + arr[:,2]**2 )
+    arr[:,0] /= lens
+    arr[:,1] /= lens
+    arr[:,2] /= lens
