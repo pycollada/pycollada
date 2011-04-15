d1f893092c57a9bee1a22e33002009cb700b7ca9
Jeff Terrace <jterrace@gmail.com>
2011-04-15 18:10:19 -0400

    Updated readme to point to new documentation and updating setup.py with new version number

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 
 setup(
     name = "pycollada",
-    version = "0.1",
+    version = "0.2",
     description = "python library for reading and writing collada documents",
     author = "Jeff Terrace and contributors",
     author_email = 'jterrace@gmail.com',
     platforms=["any"],
     license="BSD",
     install_requires=['lxml', 'numpy'],
-    url = "https://github.com/pycollada/pycollada",
+    url = "http://pycollada.github.com/",
     test_suite = "collada.tests",
     packages = find_packages()
 )
