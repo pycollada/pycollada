5b6d89944db6ada9b43fd4fbeafcfdece3ec9570
Jeff Terrace <jterrace@gmail.com>
2011-11-08 21:50:17 -0500

    Change unittest runner to return non-zero code on test failures

--- collada/__main__.py
+++ collada/__main__.py
@@ -18,4 +18,7 @@ from collada.util import unittest
 
 if __name__ == '__main__':
     suite = unittest.TestLoader().discover("tests")
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    ret = unittest.TextTestRunner(verbosity=2).run(suite)
+    if ret.wasSuccessful():
+        sys.exit(0)
+    sys.exit(1)
