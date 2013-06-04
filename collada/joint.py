115ba5675f1124c51b0c3ce11531e8c8493ca3c1
Michael Craig <mic@mit.edu>
2013-06-04 12:18:41 +0900

    prismatic and revolute joints

--- collada/joint.py
+++ collada/joint.py
@@ -16,9 +16,89 @@ from .common import DaeIncompleteError, DaeBrokenRefError, DaeMalformedError, Da
 from .xmlutil import etree as ElementTree
 from .extra import Extra
 
+def _loadValuesOfPrismaticOrRevolute( collada, localscope, node ):
+    sid = node.get('sid')
+    axis_node = node.find(tag('axis'))
+    if axis_node is None:
+        axis_text = axis_node.text
+    else:
+        axis_text = None
+    min_text = None
+    max_text = None
+    limits_node = node.find(tag('limits'))
+    if limits_node is None:
+        min_node = limits_node.find(tag('min'))
+        if min_node is not None:
+            min_text = min_node.text
+        max_node = limits_node.find(tag('max'))
+        if max_node is not None:
+            max_text = max_node.text
+
+    return (sid, axis_text, min_text, max_text)
+
+class Prismatic(DaeObject):
+    """A class containing the data coming from a COLLADA <prismatic> tag"""
+    def __init__(self, sid, axis=None, min_limit=None, max_limit=None, xmlnode=None):
+        """Create <prismatic>
+
+        FIXME
+        """
+        self.sid = sid
+        self.axis = axis
+        self.min_limit = min_limit
+        self.max_limit = max_limit
+
+        if xmlnode != None:
+            self.xmlnode = xmlnode
+        else:
+            self.xmlnode = E.prismatic()
+            self.save(0)
+
+    # NOTE: ignoring sids for axis, min, and max
+    def getchildren(self):
+        return []
+
+    # NOTE: ignoring sids for axis, min, and max
+    @staticmethod
+    def load( collada, localscope, node ):
+        (sid, axis_text, min_text, max_text) = _loadValuesOfPrismaticOrRevolute(collada, localscope, node)
+        node = Prismatic(sid, axis=axis_text, min_limit=min_text, max_limit=max_text, xmlnode=node)
+        collada.addSid(sid, node)
+        return node
+
+class Revolute(DaeObject):
+    """A class containing the data coming from a COLLADA <revolute> tag"""
+    def __init__(self, sid, axis=None, min_limit=None, max_limit=None, xmlnode=None):
+        """Create <revolute>
+
+        FIXME
+        """
+        self.sid = sid
+        self.axis = axis
+        self.min_limit = min_limit
+        self.max_limit = max_limit
+
+        if xmlnode != None:
+            self.xmlnode = xmlnode
+        else:
+            self.xmlnode = E.prismatic()
+            self.save(0)
+
+    # NOTE: ignoring sids for axis, min, and max
+    def getchildren(self):
+        return []
+
+    # NOTE: ignoring sids for axis, min, and max
+    @staticmethod
+    def load( collada, localscope, node ):
+        (sid, axis_text, min_text, max_text) = _loadValuesOfPrismaticOrRevolute(collada, localscope, node)
+        node = Revolute(sid, axis=axis_text, min_limit=min_text, max_limit=max_text, xmlnode=node)
+        collada.addSid(sid, node)
+        return node
+
 class Joint(DaeObject):
     """A class containing the data coming from a COLLADA <joint> tag"""
-    def __init__(self, id, sid, name, extras=None, xmlnode=None):
+    def __init__(self, id, sid, name, prismatics=None, revolutes=None, extras=None, xmlnode=None):
         """Create <joint>
         
         :param str id:
@@ -35,6 +115,14 @@ class Joint(DaeObject):
         self.extras = []
         if extras is not None:
             self.extras = extras
+
+        self.prismatics = []
+        if prismatics is not None:
+            self.prismatics = prismatics
+
+        self.revolutes = []
+        if revolutes is not None:
+            self.revolutes = revolutes
             
         if xmlnode != None:
             self.xmlnode = xmlnode
@@ -48,11 +136,18 @@ class Joint(DaeObject):
         id = node.get("id")
         sid = node.get("sid")
         name = node.get("name")
+        prismatic_nodes = node.findall(tag('prismatic'))
+        prismatics = [Prismatic.load(collada, localscope, pnode) for pnode in prismatic_nodes]
+        revolute_nodes = node.findall(tag('revolute'))
+        revolutes = [Revolute.load(collada, localscope, rnode) for rnode in revolute_nodes]
         extras = Extra.loadextras(collada, node)
-        node = Joint(id, sid, name, extras, xmlnode=node )
+        node = Joint(id, sid, name, prismatics=prismatics, revolutes=revolutes, extras=extras, xmlnode=node)
         collada.addId(id, node)
         collada.addSid(sid, node)
         return node
+
+    def getchildren(self):
+        return self.extras + self.prismatics + self.revolutes
 
     def save(self, recurse=True):
         Extra.saveextras(self.xmlnode,self.extras)
