81423e81064c377d8e31e2a30d049fb208f32235
Laurie O <laurie_opperman@hotmail.com>
2024-03-14 16:14:50 +1000

    Move project metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,28 +1,5 @@
-import sys
-from setuptools import find_packages, setup
-
-install_requires = ['python-dateutil>=2.2', 'numpy']
-
-if sys.version_info[0] < 3:
-    import unittest
-    if not hasattr(unittest.TestCase, "assertIsNone"):
-        install_requires.append('unittest2')
+from setuptools import setup
 
 setup(
-    name="pycollada",
-    version="0.8",
-    description="python library for reading and writing collada documents",
-    author="Jeff Terrace and contributors",
-    author_email='jterrace@gmail.com',
-    platforms=["any"],
-    license="BSD",
-    install_requires=install_requires,
-    extras_require={
-        'prettyprint': ["lxml"],
-        'validation': ["lxml"]
-    },
-    url="http://pycollada.readthedocs.org/",
     test_suite="collada.tests",
-    packages=find_packages(),
-    package_data={'collada': ['resources/*.xml']}
 )
